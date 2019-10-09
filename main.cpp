7c5a0b48be9cc35d121279e1f5306408acc4dc93
llevy00 <llevy@mail.csuchico.edu>
2019-10-09 16:14:38 -0700

    added list

--- main.cpp
+++ main.cpp
@@ -35,11 +35,11 @@ int main()
 
   }while(input != 'Q' && input != 'q');
 
-  cout<<"1 "<<list[0]<<endl;
-  cout<<"2 "<<list[1]<<endl;
-  cout<<"3 "<<list[2]<<endl;
-  cout<<"4 "<<list[3]<<endl;
-  cout<<"5 "<<list[4]<<endl;
+  cout<<"\n1 "<<list[0];
+  cout<<"\n2 "<<list[1];
+  cout<<"\n3 "<<list[2];
+  cout<<"\n4 "<<list[3];
+  cout<<"\n5 "<<list[4]<<endl;
 
   return 0;
 }
